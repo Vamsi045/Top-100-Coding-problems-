7f19db5f8628eea0da14c237e08b96ed0a52625f
Vamsi045 <63848698+Vamsi045@users.noreply.github.com>
2022-09-21 19:16:00 +0530

    First Problem

---
+++ PostiveOrNot.java
@@ -0,0 +1,13 @@
+// Positive or Negative number
+public class PostiveOrNot {
+    public static void main(String[] args) {
+        int n=11;
+        if(n<0){
+            System.out.println("The given number is negative");
+        }else if(n>0){
+            System.out.println("The given number is positive");
+        }else{
+            System.out.println("The given number is equals to zero");
+        }
+    }
+}
